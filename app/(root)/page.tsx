5f79b9dc3fee55ce6529e09cda6d7648e6836ce5
nhungbui <nhungbth3@msb.com.vn>
2025-10-20 15:46:00 +0700

    init web

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@/components/ui/button';
 
+const delay = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
 
-const HomePage = () => {
+const HomePage = async () => {
+  await delay(2000);
   return <Button>Button</Button>;
 };
 
