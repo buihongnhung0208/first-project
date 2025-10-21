dd9203c4c94c60334177597adb2b134058e601c3
nhungbui <nhungbth3@msb.com.vn>
2025-10-21 17:03:52 +0700

    done 2.8

---
+++ .next/server/chunks/ssr/app_loading_tsx_b05711b0._.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/app/loading.tsx [app-rsc] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=app_loading_tsx_b05711b0._.js.map
---
+++ .next/static/chunks/app_(root)_layout_tsx_7084cfac._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7b1785de._.js",
+  "static/chunks/_3d27207c._.js"
+],
+    source: "dynamic"
+});
