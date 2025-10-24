db6957f0d0ccf868a30ab65d8e6640b35b9aadb2
Quoc Dat <quocdattranhuu1606@gmail.com>
2025-10-24 13:46:04 +0700

    Build Product Detail UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,22 @@
 import { Button } from '@/components/ui/button';
 import ProductList from '@/components/shared/product/product-list';
-import {sampleData} from '@/db/sample-data';
+import { prisma } from '@/db/prisma';
 
 const delay = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
 
 const HomePage = async () => {
   await delay(2000);
+  
+  // Lấy sản phẩm từ database
+  const products = await prisma.product.findMany({
+    take: 8,
+    orderBy: { createdAt: 'desc' }
+  });
+
   return (
     <div className='space-y-8'>
-      <h2 className='h2-bold'>Latest Products</h2>
-      <ProductList title='Newest Arrivals' data={sampleData.products} limit={4} />
+      <h2 className='h2-bold'>Sản phẩm mới nhất</h2>
+      <ProductList title='Sản phẩm nổi bật' data={products} limit={8} />
     </div>
   );
 };
